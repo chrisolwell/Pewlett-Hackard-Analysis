fd9719535eaa54664c6e82775d7f67f79cbaa319
Chris Olwell <chrisolwell@Chriss-MacBook-Pro.local>
2022-02-15 17:24:04 -0600

    just saving progress.

--- Schema.sql
+++ Schema.sql
@@ -15,7 +15,7 @@ CREATE TABLE employees (
 	PRIMARY KEY (emp_no)
 );
 CREATE TABLE dept_manager (
-dept_no VARCHAR(4) NOT NULL,
+	dept_no VARCHAR(4) NOT NULL,
 	emp_no INT NOT NULL,
 	from_date DATE NOT NULL,
 	to_date DATE NOT NULL,
@@ -37,8 +37,12 @@ CREATE TABLE dept_emp (
 	from_date DATE NOT NULL,
 	to_date DATE NOT NULL,
 	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-	PRIMARY KEY (emp_no)
+	FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
+	PRIMARY KEY (emp_no, dept_no)
 );
+
+DROP TABLE dept_emp; 
+
 CREATE TABLE titles (
 	emp_no INT NOT NULL,
 	title VARCHAR NOT NULL,
@@ -75,9 +79,9 @@ FROM employees
 WHERE birth_date BETWEEN '1954-01-01' AND '1954-12-31';
 
 
-SELECT first_name, last_name
-FROM employees
-WHERE birth_date BETWEEN '1955-01-01' AND '1955-12-31';
+-- SELECT first_name, last_name
+-- FROM employees
+-- WHERE birth_date BETWEEN '1955-01-01' AND '1955-12-31';
 
 SELECT first_name, last_name
 FROM employees
@@ -96,3 +100,71 @@ WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
 AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
 
 SELECT * FROM retirement_info;
+
+DROP TABLE retirement_info;
+
+--Create new table for retiring employees
+SELECT emp_no, first_name, last_name
+INTO retirement_info
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31'); 
+--Check the Table
+SELECT * FROM retirement_info;
+
+--Joining departments and dpet_manager tables
+SELECT d.dept_name,
+	dm.emp_no,
+	dm.from_date,
+	dm.to_date
+FROM departments as d
+INNER JOIN dept_manager as dm
+ON d.dept_no = dm.dept_no;
+
+--Joining retirement info and dept emp tables
+SELECT retirement_info.emp_no,
+	retirement_info.first_name,
+	retirement_info.last_name,
+	dept_emp.to_date
+FROM retirement_info
+LEFT JOIN dept_emp
+ON retirement_info.emp_no = dept_emp.emp_no;
+
+--Using aliases on the same join as above
+SELECT ri.emp_no,
+	ri.first_name,
+ri.last_name,
+	de.to_date
+FROM retirement_info as ri
+LEFT JOIN dept_emp as de
+ON ri.emp_no = de.emp_no;
+
+SELECT ri.emp_no,
+	ri.first_name,
+	ri.last_name,
+de.to_date
+INTO current_emp
+FROM retirement_info as ri
+LEFT JOIN dept_emp as de
+ON ri.emp_no = de.emp_no
+WHERE de.to_date = ('9999-01-01');
+
+--EMployee count by dept number
+SELECT COUNT(ce.emp_no), de.dept_no
+INTO dept_ret
+FROM current_emp as ce
+LEFT JOIN dept_emp as de
+ON ce.emp_no = de.emp_no
+GROUP BY de.dept_no
+ORDER BY de.dept_no;
+
+-- CREATE TABLE dept_ret(
+-- 	dept_no VARCHAR (4) NOT NULL,
+-- 	eligible INT NOT NULL,
+-- 	PRIMARY KEY (dept_no)
+-- ); 
+-- DROP TABLE dept_ret;
+--SELECT * FROM dept_ret;
+SELECT * FROM salaries
+ORDER BY to_date DESC;
+-- SELECT COUNT()
